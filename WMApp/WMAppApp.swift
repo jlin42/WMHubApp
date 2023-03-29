303feb49959aed945c3a5ee8c436974ee33af6b5
Jerry Lin <jlf232003s@gmail.com>
2023-03-29 19:03:45 -0400

    Initial Commit

---
+++ WMApp/WMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WMAppApp.swift
+//  WMApp
+//
+//  Created by Jerry Lin on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
